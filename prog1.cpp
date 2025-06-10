2cd2fbc59bafa285f9097792bb209fc393ff5d3f
pratham-9029 <jaysinhdarbar828@gmail.com>
2025-06-10 10:21:38 +0530

    upd

--- prog1.cpp
+++ prog1.cpp
@@ -1,48 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void PrintArray(int [] , int);
-void BubbleSort(int [] , int); 
+void PrintArray(int[], int);
+void BubbleSort(int[], int);
 
-int main(){
+int main()
+{
 
     int n;
-    cout<<"Enter size of an array : ";
-    cin>>n;
-    
+    cout << "Enter size of an array : ";
+    cin >> n;
+
     int array[n];
 
-    for(int i=0 ; i<n ; i++){
-        cout<<"Enter array of element["<<i<<"] : ";
-        cin>>array[i];
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Enter array of element[" << i << "] : ";
+        cin >> array[i];
     }
 
-    PrintArray(array , n);
-    cout<<endl;
-    BubbleSort(array , n);
-    cout<<endl;
-    PrintArray(array , n);
-    cout<<endl;
-
-
-    
+    PrintArray(array, n);
+    cout << endl;
+    BubbleSort(array, n);
+    cout << endl;
+    PrintArray(array, n);
+    cout << endl;
 }
 
-void PrintArray(int array[] , int n){
-    for(int i = 0 ; i < n ; i++ ){
-        cout<<array[i]<<" ";
+void PrintArray(int array[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << array[i] << " ";
     }
 }
 
-void BubbleSort(int array[] , int n){
-    int i , j ;
-    for(i=0 ; i<n ; i++){
-        for(j=0 ; j<n-i-1 ; j++){
-            if(array[i]>array[i+1]){
-                swap(array[i] , array[i+1]);
+void BubbleSort(int array[], int n)
+{
+    int i, j;
+    for (i = 0; i < n ; i++)
+    {
+        for (j = 0; j < n - i ; j++)
+        {
+            if (array[j] > array[j + 1])
+            {
+                swap(array[j], array[j + 1]);
             }
         }
     }
-} 
-
-
+}
